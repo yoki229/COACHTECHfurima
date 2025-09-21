872aa7515873f2834d54664ff27673bbc0183648
kudou mai <yoki229@gmail.com>
2025-09-22 06:17:13 +0900

    シーダー作成まで

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        User::factory(10)->create();
+        
+        //テストログイン用の一人分のテストダミーデータ
+        $testUser = [
+            'name' => '山田太郎',
+            'email' => 'yamada@example.com',
+            'password' => bcrypt('password'),
+            'profile_image' => 'storage/test_image/user_sample.png',
+            'postal_code' => '123-4567',
+            'address' => '群馬県富岡市南後箇1000',
+            'building' => 'ユースパーク101',
+        ];
+        DB::table('users')->insert($testUser);
+
+    }
+}
