7c77fba07365db12edc97b0f546de2244eab7128
kudou mai <yoki229@gmail.com>
2025-10-09 22:14:40 +0900

    ヘッダーの修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,42 +11,42 @@
 </head>
 
 <body class="body">
-  <div class="app">
+    <div class="app">
 
-    <header class="header">
-      <div class="header-nav__logo">
-        <a class="header-nav__logo-link" href="/">
-          <img src="{{ asset('images/logo.svg') }}" alt="COACHTECH" class="header__img">
-        </a>
-      </div>
-      @if (Auth::check())
-      <div class="header-nav__search">
-          <form action="/search" method="get">
-              <input class="search-box" type="search" name="keyword" placeholder="なにをお探しですか？" value="{{request('keyword')}}">
-          </form>
-      </div>
-      <ul class="header-nav__list">
-          <li>
-              <form action="/logout" method="post">
-              @csrf
-                  <button class="list__logout">ログアウト</button>
-              </form>
-          </li>
-          <li>
-              <a href="/mypage" class="list__mypage">マイページ</a>
-          </li>
-          <li>
-              <a href="/sell" class="list__sellpage">出品</a>
-          </li>
-      </ul>
-      @endif
-    </header>
+        <header class="header">
+        <div class="header-nav__logo">
+            <a class="header-nav__logo-link" href="/">
+            <img src="{{ asset('images/logo.svg') }}" alt="COACHTECH" class="header__img">
+            </a>
+        </div>
+        @if (Auth::check())
+        <div class="header-nav__search">
+            <form action="/search" method="get">
+                <input class="search-box" type="search" name="keyword" placeholder="なにをお探しですか？" value="{{request('keyword')}}">
+            </form>
+        </div>
+        <ul class="header-nav__list">
+            <li>
+                <form action="/logout" method="post">
+                @csrf
+                    <button class="list__logout">ログアウト</button>
+                </form>
+            </li>
+            <li>
+                <a href="/mypage" class="list__mypage">マイページ</a>
+            </li>
+            <li>
+                <a href="/sell" class="list__sellpage">出品</a>
+            </li>
+        </ul>
+        @endif
+        </header>
 
-    <div class="content">
-      @yield('content')
+        <div class="content">
+        @yield('content')
+        </div>
+
     </div>
-
-  </div>
 </body>
 
 </html>
