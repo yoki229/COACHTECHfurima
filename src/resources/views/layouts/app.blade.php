01b21d1c833e5543cd76f6daf0d15279547beba1
kudou mai <yoki229@gmail.com>
2025-10-13 06:17:31 +0900

    インデックスページを非人称ユーザーからも見えるようにミドルウエアから外しました

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -20,27 +20,38 @@
                     <img src="{{ asset('images/logo.svg') }}" alt="COACHTECH" class="header__img">
                 </a>
             </div>
+
             @if (Auth::check())
-            <div class="header-nav__search">
-                <form action="/search" method="get">
-                    <input class="search-box" type="search" name="keyword" placeholder="なにをお探しですか？" value="{{request('keyword')}}">
-                </form>
-            </div>
-            <ul class="header-nav__list">
-                <li>
-                    <form action="/logout" method="post">
-                    @csrf
-                        <button class="list__logout">ログアウト</button>
+                <div class="header-nav__search">
+                    <form action="/search" method="get">
+                        <input class="search-box" type="search" name="keyword" placeholder="なにをお探しですか？" value="{{request('keyword')}}">
                     </form>
-                </li>
-                <li>
-                    <a href="/mypage" class="list__mypage">マイページ</a>
-                </li>
-                <li>
-                    <a href="/sell" class="list__sellpage">出品</a>
-                </li>
-            </ul>
+                </div>
+                <ul class="header-nav__list">
+                    <li>
+                        <form action="/logout" method="post">
+                        @csrf
+                            <button class="list__logout">ログアウト</button>
+                        </form>
+                    </li>
+                    <li>
+                        <a href="/mypage" class="list__mypage">マイページ</a>
+                    </li>
+                    <li>
+                        <a href="/sell" class="list__sellpage">出品</a>
+                    </li>
+                </ul>
+            
+            @else
+                <ul class="header-nav__list">
+                    @if (request()->path() !== 'login')
+                        <li>
+                            <a href="/login" class="list__login">ログインする</a>
+                        </li>
+                    @endif
+                </ul>
             @endif
+
         </header>
 
         <!-- コンテンツ -->
