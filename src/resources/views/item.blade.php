64d044fcf0a5cde0c0e33664bc5fdc6ec9badf45
Kudou mai <yoki229@gmail.com>
2025-10-14 22:52:20 +0900

    item詳細ページ製作中

--- src/resources/views/item.blade.php
+++ src/resources/views/item.blade.php
@@ -5,15 +5,46 @@
 @endsection
 
 @section('content')
+<div class="container">
+    <div class="container__item">
+        {{-- 商品画像 --}}
+        <div class="item-image {{ $item->sold_class }}">
+            <img src="{{ asset($item->image) }}" alt="商品画像" class="item__img" />
+        </div>
+    </div>
 
-<form class="like-button" action="/item/{{ $item->id }}/like" method="post">
-    @csrf
-    @if($item->liked)
-        <button type="submit" class="like-button--liked">★</button>
-    @else
-        <button type="submit" class="like-button--none">☆</button>
-    @endif
-</form>
+    <div class="container__item">
+        {{-- 商品名 --}}
+        <div class="item-detail__name">
+            <p class="item-name">{{$item->name}}</p>
+            @if ($item->buyer_id)
+                <span class="sold">sold</span>
+            @endif
+        </div>
 
+        {{-- ブランド名 --}}
 
+        {{-- 値段 --}}
+
+        {{-- いいね機能・いいね数 --}}
+        <form class="like-button" action="/item/{{ $item->id }}/like" method="post">
+            @csrf
+            @if($item->liked)
+                <button type="submit" class="like-button--liked">★</button>
+            @else
+                <button type="submit" class="like-button--none">☆</button>
+            @endif
+        </form>
+
+        {{-- コメント数 --}}
+
+        {{-- 購入手続きボタン --}}
+
+        {{-- 商品説明 --}}
+
+        {{-- 商品の情報 --}}
+
+        {{-- コメント欄 --}}
+    </div>
+</div>
 @endsection
