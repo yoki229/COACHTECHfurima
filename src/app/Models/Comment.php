2bcf61abd16633a4c0bcb38acd6e4c078e632e78
Kudou mai <yoki229@gmail.com>
2025-10-18 19:17:00 +0900

    item詳細ページ機能〇

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -10,8 +10,19 @@ class Comment extends Model
     use HasFactory;
 
     protected $fillable = [
-        'content',
+        'comment',
         'user_id',
         'item_id',
     ];
+
+    //リレーション
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
 }
