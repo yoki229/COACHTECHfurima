3cde1aeb9b8f4567186474c8754bcc800830c04a
Kudou mai <yoki229@gmail.com>
2025-10-19 17:44:11 +0900

    決済画面まだ途中（一旦セーブ）

--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -6,82 +6,84 @@
 
 @section('content')
 <div class="container">
-<form action="/purchase/{{ $item->id }}" method="POST">
+    <form action="/purchase/{{ $item->id }}" method="post">
     @csrf
-    <div class="container-inner">
-        <div class="container__left">
-            {{-- 商品 --}}
-            <div class="item">
-                {{-- 商品画像 --}}
-                <div class="item-image">
-                    <img src="{{ asset($item->image) }}" alt="商品画像" class="item__img" />
+        <div class="container-inner">
+            {{-- 左側 --}}
+            <div class="container__left">
+                {{-- 商品 --}}
+                <div class="item">
+                    {{-- 商品画像 --}}
+                    <div class="item-image">
+                        <img src="{{ asset($item->image) }}" alt="商品画像" class="item__img" />
+                    </div>
+
+                    {{-- 商品名と値段 --}}
+                    <div class="item-detail">
+                        <h1 class="item-name">{{ $item->name }}</h1>
+
+                        <div class="item-detail__price">
+                            <span class="item-price">￥</span><span class="item-price-number">{{ number_format($item->price) }}</span>
+                        </div>
+                    </div>
                 </div>
 
-                {{-- 商品名 --}}
-                <div class="item-detail__name">
-                    <h1 class="item-name">{{ $item->name }}</h1>
-                    @if ($item->buyer_id)
-                        <span class="sold">sold</span>
-                    @endif
+                <hr>
+
+                {{-- 支払い方法 --}}
+                <div class="pay">
+                    <h3 class="pay-title">支払い方法</h3>
+                    <select name="payment_method" id="payment_id" class="pay-select" required>
+                        <option value="" selected hidden>選択してください</option>
+                        @foreach($payments as $pay => $label)
+                            <option value="{{ $pay }}">{{ $label }}</option>
+                        @endforeach
+                    </select>
+                    {{-- バリデーションエラー表示 --}}
+                    @error('payment_method')
+                        <p class="error-message">{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- 商品値段 --}}
-                <div class="item-detail__price">
-                    <span class="item-price">￥</span>
-                    <span class="item-price-number">{{ number_format($item->price) }}</span>
+                <hr>
+
+                {{-- 配送先 --}}
+                <div class="address">
+                    <h3 class="address-title">配送先</h3>
+                    <div class="address-list">
+                        <input type="text" class="" name="postal_code" value="〒{{ $user->postal_code }}" readonly>
+                        <input type="text" class="" name="address" value="{{ $user->address }}" readonly>
+                        <input type="text" class="" name="building" value="{{ $user->building }}" readonly>
+                    </div>
+
+                    <a href="/purchase/address/{{ $item->id }}">変更する</a>
+                </div>
+                <hr>
+            </div>
+
+            {{-- 右側 --}}
+            <div class="container__right">
+                {{-- 決済確認 --}}
+                <table class="sell-table">
+                    <tr>
+                        <th>商品代金</th>
+                        <td>
+                            <span class="item-price">￥</span><span class="item-price-number">{{ number_format($item->price) }}</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>支払方法</th>
+                        <td id="payment-preview">未選択</td>
+                    </tr>
+                </table>
+
+                {{-- 購入ボタン --}}
+                <div class="sell-button">
+                    <button type="submit" class="sell-button__button">購入する</button>
                 </div>
             </div>
-            <hr>
-            {{-- 支払い方法 --}}
-            <div class="pay">
-                <label for="payment_id" class="pay-title">
-                    支払い方法
-                </label>
-                <select name="payment_id" id="payment_id" class="pay-select" required>
-                    <option value="" disabled selected>選択してください</option>
-                    @foreach($payments as $pay => $label)
-                        <option value="{{ $pay }}">{{ $label }}</option>
-                    @endforeach
-                </select>
-                {{-- バリデーションエラー表示 --}}
-                @error('payment_id')
-                    <p class="error-message">{{ $message }}</p>
-                @enderror
-            </div>
-            <hr>
-            {{-- 配送先 --}}
-            <div class="address">
-                <h3>配送先</h3>
-                <p>〒{{ $user->postal_code }}</p>
-                <p>{{ $user->address }}</p>
-                <p>{{ $user->building }}</p>
-
-                <a href="/purchase/address/{{ $item->id }}">変更する</a>
-            </div>
-            <hr>
         </div>
-
-        <div class="container__right">
-            {{-- 決済確認 --}}
-            <table class="sell-table">
-                <tr>
-                    <th>商品代金</th>
-                    <td>{{ number_format($item->price) }}</td>
-                </tr>
-                <tr>
-                    <th>支払方法</th>
-                    <td id="payment-preview">未選択</td>
-                </tr>
-            </table>
-
-            {{-- 購入ボタン --}}
-            <div class="sell-button">
-                <button type="submit" class="sell-button__button">購入する</button>
-            </div>
-
-        </div>
-    </div>
-</form>
+    </form>
 </div>
 
 <script>
