002a3f9cabb3c02f1528d757ddc914d896f84ee7
Kudou mai <yoki229@gmail.com>
2025-10-22 06:02:43 +0900

    だいたい購入画面完成

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,24 +4,37 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', [ItemController::class, 'index']);      //商品一覧画面（おすすめ画面）
-Route::get('/item/{item_id}', [ItemController::class, 'getItem']);               //商品詳細画面
-Route::post('/item/{item_id}/comments_store', [ItemController::class, 'commentsStore']);               //コメント投稿機能
+//商品一覧画面（おすすめ画面）の表示
+Route::get('/', [ItemController::class, 'index']);
+//商品詳細画面の表示
+Route::get('/item/{item_id}', [ItemController::class, 'getItem']);
+//コメント投稿機能
+Route::post('/item/{item_id}/comments_store', [ItemController::class, 'commentsStore']);
 
-//あとでミドルウエアにいれる
-Route::get('/mypage_profile', [ProfileController::class, 'mypageProfile']);     //プロフィール編集画面
-Route::post('/update_profile', [ProfileController::class, 'updateProfile']);    //プロフィール更新後処理用
-Route::get('/mypage', [ProfileController::class, 'mypage']);                    //プロフィール画面
-Route::get('/purchase/address/{item_id}',[ProfileController::class, 'editAddress']);                           //住所変更画面
-Route::post('/purchase/address/{item_id}', [ProfileController::class, 'updateAddress']);       //住所変更機能
-Route::post('/item/{item_id}/like', [ItemController::class, 'like']);       //いいね機能
-Route::get('/search', [ItemController::class, 'search']);                       //検索処理用
-Route::get('/purchase/{item_id}', [ItemController::class, 'purchase']);                           //商品購入画面
-Route::post('/purchase/{item_id}', [ItemController::class, 'store']);                           //商品購入機能
-Route::get('/sell', [ItemController::class, 'sell']);                           //商品出品画面
-
-
+//ログイン済み用のルーティング
 Route::middleware('auth')->group(function () {
-    Route::get('/mylist', [ItemController::class, 'mylist']);          //商品画面一覧（マイリスト画面）
+    //商品画面一覧（マイリスト画面）の表示
+    Route::get('/mylist', [ItemController::class, 'mylist']);
+    //プロフィール編集画面の表示
+    Route::get('/mypage_profile', [ProfileController::class, 'mypageProfile']);
+    //プロフィール更新処理
+    Route::post('/update_profile', [ProfileController::class, 'updateProfile']);
+    //プロフィール画面の表示
+    Route::get('/mypage', [ProfileController::class, 'mypage']);
+    //住所変更画面の表示
+    Route::get('/purchase/address/{item_id}',[ProfileController::class, 'editAddress']);
+    //住所変更処理
+    Route::post('/purchase/address/{item_id}', [ProfileController::class, 'updateAddress']);
+
+    //いいね機能
+    Route::post('/item/{item_id}/like', [ItemController::class, 'like']);
+    //検索処理
+    Route::get('/search', [ItemController::class, 'search']);
+    //商品購入画面の表示
+    Route::get('/purchase/{item_id}', [ItemController::class, 'purchase']);
+    //商品購入時処理
+    Route::post('/purchase/{item_id}', [ItemController::class, 'store']);
+    //出品ページの表示の表示
+    Route::get('/sell', [ItemController::class, 'sell']);
 
 });
